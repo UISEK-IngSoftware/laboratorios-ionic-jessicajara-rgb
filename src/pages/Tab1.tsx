03781bb87606cd3211d9e4b370b3832e0e35e556
jessicajara <jessica.jara@uisek.edu.ec>
2026-07-08 17:30:28 -0500

    Resolucion Laboratorio 7

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,66 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {
+  IonContent,
+  IonHeader,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
 import './Tab1.css';
+import RepoItem from "../components/RepoItem";
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Lista de Repositorios</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Repositorios</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+
+        <IonList>
+
+          <RepoItem
+            name="Repositorio 1"
+            description="This is a description for Repo 5."
+            language="JavaScript"
+            avatarUrl="https://avatars.githubusercontent.com/u/1?v=4"
+          />
+
+          <RepoItem
+            name="Repositorio 2"
+            description="This is a description for Repo 5."
+            language="JavaScript"
+            avatarUrl="https://avatars.githubusercontent.com/u/1?v=4"
+          />
+          <RepoItem
+            name="Repositorio 3"
+            description="This is a description for Repo 5."
+            language="Python"
+            avatarUrl="https://avatars.githubusercontent.com/u/1?v=4"
+
+          />
+          <RepoItem
+            name="Repositorio 4"
+            description="This is a description for Repo 5."
+            language="Java"
+            avatarUrl="https://avatars.githubusercontent.com/u/1?v=4"
+          />
+          <RepoItem
+            name="Repositorio 5"
+            description="This is a description for Repo 5."
+            language="TypeScript"
+            avatarUrl="https://avatars.githubusercontent.com/u/1?v=4"
+          />
+
+        </IonList>
+
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 
@@ -7,16 +7,29 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Perfil de Usuario</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Perfil de Usuario</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+
+        <div className="card-container">
+          <IonCard className="card">
+            <img src="https://avatars.githubusercontent.com/u/12345678?v=4" alt="Avatar" className="acrd-avatar" />
+          <IonCardHeader>
+            <IonCardTitle> Jessica Jara</IonCardTitle>
+            <IonCardSubtitle> jessicajara@uisek.edu.ec</IonCardSubtitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <p> Desarrollador web con experiencia en frontend y backend.
+            Apasionado por la tecnologia y el código limpio.</p>
+          </IonCardContent>
+          </IonCard>
+        </div>
       </IonContent>
     </IonPage>
   );
